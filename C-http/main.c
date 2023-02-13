9d1a3c437edd09a13bb60203ac106459e1da6980
yukiffe <yukiffe0.gmail.com>
2023-02-14 06:48:55 +0900

    base

--- C-http/main.c
+++ C-http/main.c
@@ -6,7 +6,42 @@
 #define IP = "127.0.0.1"
 #define PORT 80
 
-int main() {
+int socket() {
 
+}
+
+int bind() {
+
+}
+
+int listen() {
+
+}
+
+int accept() {
+
+}
+
+int read_write() {
+
+}
+
+int close() {
+
+}
+
+int main() {
+	WSADATA wsaData;
+	
+	while (true) {
+
+	}
 	return 0;
 }
+//tistory
+/*
+socket -> bind
+listen -> accept
+read-write
+close
+*/
