9feed11c4183fb97242aa611116b1bfbc0628d1f
yukiffe <yukiffe0.gmail.com>
2023-02-14 06:42:52 +0900

    IP, PORT define

--- C-http/main.c
+++ C-http/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <string.h>
+#include <Winsock2.h>
+#include <io.h>
 
-int main(){
+#define IP = "127.0.0.1"
+#define PORT 80
 
-    return 0;
+int main() {
+
+	return 0;
 }
