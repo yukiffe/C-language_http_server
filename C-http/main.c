cad3f125025b3466d047b375da152b1aaa6892e4
yukiffe <yukiffe0.gmail.com>
2023-02-16 00:33:14 +0900

    중간업뎃 - 코드 정리 및 일부 제거(주요기능만 남기기), http로 보낼 코드

--- C-http/main.c
+++ C-http/main.c
@@ -10,72 +10,58 @@
 #include <winsock.h>
 #include <ws2tcpip.h>
 
-#define IP = "127.0.0.1"
-#define PORT 80
-#define BUFFER 1024 
-#define MAX_CONNECT 0x00000002 
+#define PORT 3000 // 자동완성이 자꾸 3000가르켜서 변경
+#define BUFFER 1024  
 
 using namespace std;
 
-int _connection_count = 0;
-SOCKET* _connect_sockets[MAX_CONNECT];
+/*
+* C언어 API 사용 경험이 적고 코드가 너무 난잡해서
+* 자고일어나면 기억리셋 + 보기어려운 관계로
+* 쓸모 없는 코드를 제거하고 다시 쌓아갈 예정
+* -> 서버의 실행, 클라이언트에 http 헤더+바디를 제공하는걸 최우선으로 지정
+*/
+SOCKET* _connect_sockets;
 
+void error(const char* _message);
+void init(WSADATA* _window_socket_API_data, SOCKET* _server_socket);
+void run(SOCKET* _server_socket);
 
-void error(const char* _message) {
+int main() {
+	WSADATA _window_socket_API_data;
+	SOCKET _server_socket;//listen->service로 의미 변경
+	init(&_window_socket_API_data, &_server_socket);
+	run(&_server_socket);
+	return 0;
+}	void error(const char* _message) {
 	printf("ERROR: \"%s\"\n", _message);
 	exit(-1);
 }
 
-void init(WSADATA* _window_socket_API_data, SOCKET* _service_socket, int* _return_value) {
+void init(WSADATA* _window_socket_API_data, SOCKET* _server_socket) {
 	if (WSAStartup(MAKEWORD(2, 2), _window_socket_API_data) != 0) error("Start-Up");
-	*_service_socket = socket(AF_INET, SOCK_STREAM, 0);
-	if (*_service_socket == INVALID_SOCKET) error("Invalid-Socket");
-	SOCKADDR_IN _listener_address;
-	ZeroMemory(&_listener_address, sizeof(SOCKADDR_IN));
-	_listener_address.sin_family = AF_INET;
-	_listener_address.sin_port = htons(PORT);
-	_listener_address.sin_addr.s_addr = htonl(INADDR_ANY);
-	if (bind(*_service_socket, (SOCKADDR*)&_listener_address, sizeof(_listener_address)) == SOCKET_ERROR)error("Binding");
-	if (listen(*_service_socket, MAX_CONNECT) == SOCKET_ERROR) error("Listening");
-	u_long _on = 1;
-	*_return_value = ioctlsocket(*_service_socket, FIONBIO, &_on);
-	if (*_return_value == SOCKET_ERROR) error("Asynchronous");
+	*_server_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (*_server_socket == INVALID_SOCKET) error("Invalid-Socket");
+	SOCKADDR_IN _service_address;
+	ZeroMemory(&_service_address, sizeof(SOCKADDR_IN));
+	_service_address.sin_family = AF_INET;
+	_service_address.sin_port = htons(PORT);
+	_service_address.sin_addr.s_addr = htonl(INADDR_ANY);
+	if (bind(*_server_socket, (SOCKADDR*)&_service_address, sizeof(_service_address)) == -1)error("Binding");
+	if (listen(*_server_socket, 10) == -1) error("Listening");
 }
 
-void run(SOCKET* _service_socket, int* _return_value) {
-	FD_SET _fd_set;
+
+void run(SOCKET* _server_socket) {
 	SOCKET _client_socket;
 	SOCKADDR_IN _client_address;
-	FD_ZERO(&_fd_set);
-	FD_SET(*_service_socket, &_fd_set);
-	for (int i = 0; i < _connection_count; i++) {
-		FD_SET(*_connect_sockets[i], &_fd_set);
-	}
-	*_return_value = select(0, &_fd_set, nullptr, nullptr, nullptr);
-	if (*_return_value == SOCKET_ERROR) {
-		error("Select (Read/Write)");
-	}
-	if (FD_ISSET(*_service_socket, &_fd_set)) {
-		int client_address_length = sizeof(_client_address);
-		_client_socket = accept(*_service_socket, (SOCKADDR*)&_client_address, &client_address_length);
-		printf("connect: %s\n", inet_ntoa(_client_address.sin_addr));
-		if (_connection_count < MAX_CONNECT) {
-			SOCKET* socket = new SOCKET;
-			_connect_sockets[_connection_count++] = socket;
-		}
-		else {
-			printf("연결 초과");
-		}
-	}
-}
+	int _client_address_length = sizeof(_client_address);
+	socklen_t client_length;
+	char* buffer = "Hello, World!";
 
-int main() {
-	WSADATA _window_socket_API_data;
-	SOCKET _service_socket;//listen->service로 의미 변경
-	int _return_value;
-	init(&_window_socket_API_data, &_service_socket, &_return_value);
-	while (true) {
-		run(&_service_socket, &_return_value);
-	}
-	return 0;
+	_client_socket = accept(*_server_socket, (SOCKADDR*)&_client_address, &_client_address_length);
+	printf("tcp 접속: %s", inet_ntoa(_client_address.sin_addr));
+	int len = sizeof(buffer);
+	send(_client_socket, buffer, sizeof(buffer), BUFFER);
+	closesocket(_client_socket);
 }
