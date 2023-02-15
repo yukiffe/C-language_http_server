971864067ddabe6f73d0de93ae91fa10117b9a1e
yukiffe <yukiffe0.gmail.com>
2023-02-16 04:44:56 +0900

    드뎌원인발견... 메모리가 문제였따..........

--- C-http/main.c
+++ C-http/main.c
@@ -10,6 +10,8 @@
 #include <winsock.h>
 #include <Windows.h>
 #include <io.h>
+#include <locale.h>
+#include <wchar.h>
 
 #define PORT 3000
 #define BUFFER 1024  
@@ -28,37 +30,32 @@ int main() {
 	SOCKADDR_IN _server_address, _client_address;
 	int _server_address_length = sizeof(_server_address);
 	int _client_address_length = sizeof(_client_address);
-	memset(&_server_address, '0', sizeof(SOCKADDR_IN));
-	memset(&_client_address, '0', sizeof(SOCKADDR_IN));
 
-	if (WSAStartup(MAKEWORD(2, 2), &_window_socket_API_data) != 0) error("Start-Up");
+	if (WSAStartup(MAKEWORD(2, 0), &_window_socket_API_data) != 0) error("Start-Up");
 	_server_socket = socket(AF_INET, SOCK_STREAM, 0);
 	if (_server_socket == INVALID_SOCKET) error("Invalid-Socket");
-	ZeroMemory(&_server_address, sizeof(SOCKADDR_IN));
 	_server_address.sin_family = AF_INET;
 	_server_address.sin_port = htons(PORT);
 	_server_address.sin_addr.s_addr = htonl(INADDR_ANY);
 	if (bind(_server_socket, (SOCKADDR*)&_server_address, _server_address_length) != 0)error("Binding");
 	if (listen(_server_socket, SOMAXCONN) != 0) error("Listening");
 
+	char header[1024];
+	memset(header, 0, sizeof(header));
+
 	while (true) {
-		_client_socket = socket(AF_INET, SOCK_STREAM, 0);
 		_client_socket = accept(_server_socket, (SOCKADDR*)(&_server_address), &_server_address_length);
 		if (_client_socket < 0) error("Accept");
-		if (getsockname(_client_socket, (SOCKADDR*)&_client_address, &_client_address_length) < 0) { error("Server"); continue; }
-		
-		printf("SOCKET3");
+
 		closesocket(_server_socket);
-		printf("SOCKET4");
-		//if (read(_client_socket, buffer,strlen(buffer)) < 0) { error("Read"); continue; }//하나만읽을꺼니까 while x
-		printf("SOCKET5");
-		char method[BUFFER], uri[BUFFER], version[BUFFER];
-		sscanf(buffer, "%s %s %s", method, uri, version);
-		printf("[%s:%u] %s %s %s\n", inet_ntoa(_client_address.sin_addr), ntohs(_client_address.sin_port), method, version, uri);
-		printf("SOCKET7");
-		if (write(_client_socket, resp, strlen(resp)) < 0) {error("Write"); continue;}
-		printf("SOCKET8");
-		close(_client_socket);
+		char szBuf[BUFFER], szInBuf[BUFFER];
+		memset(szBuf, 0, sizeof(szBuf));
+		memset(szInBuf, 0, sizeof(szInBuf));
+
+		recv(_client_socket, szInBuf, sizeof(szInBuf), 0);
+		send(_client_socket, szBuf, (int)strlen(szBuf), 0);
+
+		closesocket(_client_socket);
 	}
 
 	return 0;
