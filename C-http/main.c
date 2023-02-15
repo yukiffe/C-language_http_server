ac49032dcc5df2df1e16da734fbef1d21a5ed4d4
yukiffe <yukiffe0.gmail.com>
2023-02-16 02:43:30 +0900

    에러위치 -> write 원인찾기

--- C-http/main.c
+++ C-http/main.c
@@ -8,20 +8,19 @@
 #include <vector>
 #include <Winsock2.h>
 #include <winsock.h>
-#include <ws2tcpip.h>
+#include <Windows.h>
+#include <io.h>
 
-#define PORT 3000 // 자동완성이 자꾸 3000가르켜서 변경
+
+#define PORT 3000
 #define BUFFER 1024  
 
-using namespace std;
+//i will be die.... die..... i willl..... diie.....
 
-/*
-* C언어 API 사용 경험이 적고 코드가 너무 난잡해서
-* 자고일어나면 기억리셋 + 보기어려운 관계로
-* 쓸모 없는 코드를 제거하고 다시 쌓아갈 예정
-* -> 서버의 실행, 클라이언트에 http 헤더+바디를 제공하는걸 최우선으로 지정
-*/
-SOCKET* _connect_sockets;
+char resp[] = "HTTP/1.0 200 OK\r\n"
+"Server: webserver-c\r\n"
+"Content-type: text/html\r\n\r\n"
+"<html>hello, world</html>\r\n";
 
 void error(const char* _message);
 void init(WSADATA* _window_socket_API_data, SOCKET* _server_socket);
@@ -29,13 +28,11 @@ void run(SOCKET* _server_socket);
 
 int main() {
 	WSADATA _window_socket_API_data;
-	SOCKET _server_socket;//listen->service로 의미 변경
+	SOCKET _server_socket;
 	init(&_window_socket_API_data, &_server_socket);
-	run(&_server_socket);
+	while (true)
+		run(&_server_socket);
 	return 0;
-}	void error(const char* _message) {
-	printf("ERROR: \"%s\"\n", _message);
-	exit(-1);
 }
 
 void init(WSADATA* _window_socket_API_data, SOCKET* _server_socket) {
@@ -48,20 +45,34 @@ void init(WSADATA* _window_socket_API_data, SOCKET* _server_socket) {
 	_service_address.sin_port = htons(PORT);
 	_service_address.sin_addr.s_addr = htonl(INADDR_ANY);
 	if (bind(*_server_socket, (SOCKADDR*)&_service_address, sizeof(_service_address)) == -1)error("Binding");
-	if (listen(*_server_socket, 10) == -1) error("Listening");
+	if (listen(*_server_socket, SOMAXCONN) == -1) error("Listening");
 }
-
 
 void run(SOCKET* _server_socket) {
 	SOCKET _client_socket;
 	SOCKADDR_IN _client_address;
 	int _client_address_length = sizeof(_client_address);
-	socklen_t client_length;
-	char* buffer = "Hello, World!";
-
-	_client_socket = accept(*_server_socket, (SOCKADDR*)&_client_address, &_client_address_length);
-	printf("tcp 접속: %s", inet_ntoa(_client_address.sin_addr));
-	int len = sizeof(buffer);
-	send(_client_socket, buffer, sizeof(buffer), BUFFER);
+	_client_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (_client_socket == -1) error("Client_Socket");
+	_client_socket = accept(*_server_socket, (SOCKADDR*)(&_client_address), &_client_address_length);
+	if (_client_socket < 0) error("Accept");
+	char buffer[BUFFER];
+	//if (read(_client_socket, buffer, BUFFER) < 0)error("Read");
+	//closesocket(*_server_socket);
+	char method[BUFFER], uri[BUFFER], version[BUFFER];
+	sscanf(buffer, "%s %s %s", method, uri, version);
+	printf("[%s:%u] %s %s %s\n", inet_ntoa(_client_address.sin_addr), ntohs(_client_address.sin_port), method, version, uri);
+	try {
+		if (write(_client_socket, resp, strlen(resp)) < 0) error("Write");
+	}
+	catch(char* error) {
+		printf("ERROR");
+	}
 	closesocket(_client_socket);
+	WSACleanup();
+}
+
+void error(const char* _message) {
+	printf("ERROR: \"%s\"\n", _message);
+	exit(-1);
 }
