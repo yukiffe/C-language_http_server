838781e6c16c524ccdce0353815bcfac81c871c4
yukiffe <122760348+yukiffe@users.noreply.github.com>
2023-02-18 18:45:44 +0900

    Update sub.c
    주석 o ( 주석 추가 )

--- sub/sub.c
+++ sub/sub.c
@@ -10,7 +10,8 @@
 char buffer[BUFFER]; // 정보를 받아올 버퍼
 char resp[] = "HTTP/1.0 200 OK\r\n"
 "Server: webserver-c\r\n"
-"Content-type: text/html\r\n\r\n"
+"Content-type: text/html\r\n\r\n" // 외설, http헤더는 \r\n\r\n으로 끝난다.
+								// 이 점을 이용해 웹 해킹을 시도하기도 한다.
 "<html>hello, world</html>\r\n"; // html을 출력할 헤더와 바디
 
 void error(const char* _message); // 에러를 출력해줄 함수
@@ -22,11 +23,15 @@ int main() {
 	int _server_address_length = sizeof(_server_address); // 의 길이
 
 	if (WSAStartup(MAKEWORD(2, 0), &_window_socket_API_data) != 0) error("Start-Up"); //소켓 통신 초기화
-	_server_socket = socket(AF_INET, SOCK_STREAM, 0);  // 소켓 생성
+	// 2.0버전의 사용에 문제가 없어서 그대로 사용하였다.
+    _server_socket = socket(AF_INET, SOCK_STREAM, 0);  // 소켓 생성
+    // AF_INET = IP4v
+    // 연결 지향
+    // 0 프로토콜
 	if (_server_socket == INVALID_SOCKET) error("Invalid-Socket"); // 소켓 검증
 	_server_address.sin_family = AF_INET; // IP4 주소
 	_server_address.sin_port = htons(PORT); // 연결 포트
-	_server_address.sin_addr.s_addr = htonl(INADDR_ANY); // 주소
+	_server_address.sin_addr.s_addr = htonl(INADDR_ANY); // 프로토콜, TCP로 비연결성 유지
 	if (bind(_server_socket, (SOCKADDR*)&_server_address, _server_address_length) != 0)error("Binding"); // 서버 바인딩
 	if (listen(_server_socket, SOMAXCONN) != 0) error("Listening"); // 서버 리스닝
 
@@ -37,6 +42,9 @@ int main() {
 		printf("%s", buffer); // 콘솔 출력
 		send(_client_socket, resp, strlen(resp), 0); // 서버에서 클라이언트로 소켓 전송
 		closesocket(_client_socket); // 클라이언트 연결 해제
+        // 클라이언트와 연결 후 끊는 경우 http 통신 취급
+        // http는 전체를 주고받아 연결 유지가 필요 x
+        // 연결 해제 없이 그대로 동작 ( 연결유지는 socket 통신 )
 	}
 }
 
